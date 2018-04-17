3cdfeb210ccc6581403e56606dc3589dd6e162ca
longke <longke@cictec.cn>
2018-04-17 18:05:24 +0800

    html dom

---
+++ Readme.md
@@ -0,0 +1,4 @@
+javascript:;与javascript:void(0)使用介绍\
+https://www.w3cschool.cn/xqw2e7/by4m12xr.html\
+https://www.w3cschool.cn/javascript/js-function-definition.html\
+绝对不要在文档加载完成之后使用 document.write()。这会覆盖该文档。
--- assets/js/my.js
+++ assets/js/my.js
@@ -3,7 +3,7 @@ function ok() {
 }
 
 var carName="Volvo";
-document.getElementById("demo").innerHTML=carName;
+document.getElementById("demo1").innerHTML = carName;
 
 function getStringCode(){
     var name='daniel';
@@ -11,6 +11,7 @@ function getStringCode(){
    alert(code);
 }
 
+//js捕获异常
 var txt="";
 function message()
 {
@@ -25,4 +26,50 @@ function message()
         txt+="点击确定继续。\n\n";
         alert(txt);
     }
+}
+
+a = 5;
+b = 6;
+c = a + b;
+console.log(c);
+
+
+//js操作json
+var text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+var obj = JSON.parse(text);
+
+document.getElementById("demo").innerHTML =
+    obj.employees[1].firstName + " " + obj.employees[1].lastName;
+
+
+//函数自调用
+// (function () {
+//     document.getElementById("demo2").innerHTML = "Hello! 我是自己调用的";
+// })();
+
+//函数作为方法调用
+var myObject = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+document.getElementById("demo4").innerHTML = myObject.fullName();
+
+//函数闭包
+var add = (function () {
+    var counter = 0;
+    return function () {
+        return counter += 1;
+    }
+})();
+
+function myFunction() {
+    document.getElementById("demo5").innerHTML = add();
 }
