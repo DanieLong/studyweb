95abfaf82fd878b7d66fde3a44b9d1a5fccfcf51
daniel <mdaniell0318@gmail.com>
2018-04-17 23:29:16 +0800

    开始jquery

--- assets/js/myjquery.js
+++ assets/js/myjquery.js
@@ -2,7 +2,7 @@ function myFunction() {
     $("#h01").attr("style", "color:red").html("Hello jQuery")
 }
 
-$(document).ready(myFunction);
+// $(document).ready(myFunction);
 
 $(document).ready(function () {
     $("p").click(function () {
